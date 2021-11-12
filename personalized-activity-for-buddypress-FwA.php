0a94e1f1db1a7d70c98cdbc0928766264a120cf0
cromacio07 <93533452+cromacio07@users.noreply.github.com>
2021-11-12 09:44:12 +0530

    Create personalized-activity-for-buddypress-FwA.php

---
+++ personalized-activity-for-buddypress-FwA.php
@@ -0,0 +1,31 @@
+<?php
+/**
+* Plugin Name: Personalized Activity for Buddypress fwa
+* Description: Makes Buddypress Activity Personalized For Users, by Including Activity Feeds Only From Users They Are Following And Administrator of Your Community.
+* Version: 1.0.2
+* Author: Cromacio
+* Author URI: https://profiles.wordpress.org/crossbow6/
+* License:      GPL2
+* License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+**/
+
+function personalized_activity_for_buddypress_FfFwA_args( $args ) {
+	
+	if( ! bp_is_activity_directory() || !  is_user_logged_in() ) {
+		return $args;
+	}
+	
+	$user_id = get_current_user_id();
+	$user_admin = 1;
+	$user_idsf = bp_get_following_ids ( $user_id );
+	
+	//include users own too?
+	array_push( $user_idsf, $user_id, $user_admin);
+	
+	$args['user_id'] = $user_idsf;
+	
+	//print_r($args);
+	return $args;
+	
+}
+add_filter( 'bp_after_has_activities_parse_args', 'personalized_activity_for_buddypress_FwA_args' );
