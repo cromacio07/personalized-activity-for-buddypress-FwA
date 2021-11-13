ceae435db66f3c56d3e2ccb194224b0188a8541f
cromacio07 <93533452+cromacio07@users.noreply.github.com>
2021-11-13 10:33:52 +0530

    Update personalized-activity-for-buddypress-FwA.php

--- personalized-activity-for-buddypress-FwA.php
+++ personalized-activity-for-buddypress-FwA.php
@@ -2,30 +2,34 @@
 /**
 * Plugin Name: Personalized Activity for Buddypress - Following & Admin
 * Description: Makes Buddypress Activity Personalized For Users, by Including Activity Feeds Only From Users They Are Following And Administrator of Your Community.
-* Version: 1.0.2
-* Author: Cromacio
-* Author URI: https://profiles.wordpress.org/crossbow6/
-* License:      GPL2
-* License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+* Version: 1.0.3
+* Author: Rubix from SPARCK
+* Author URI: https://drip-sparck.pantheonsite.io
+* License:      GPL3
+* License URI:  https://www.gnu.org/licenses/gpl-3.0.html
 **/
 
-function personalized_activity_for_buddypress_FwA_args( $args ) {
+function personalized_activity_for_buddypress_FwA_by_rubix_args( $args ) {
 	
 	if( ! bp_is_activity_directory() || !  is_user_logged_in() ) {
 		return $args;
 	}
 	
-	$user_id = get_current_user_id();
-	$user_admin = 1;
-	$user_idsf = bp_get_following_ids ( $user_id );
+	//get owner's user id - by rubix	
+	$user_id_by_rubix = get_current_user_id();
 	
-	//include users own too?
-	array_push( $user_idsf, $user_id, $user_admin);
+	//includes activity from user with user id as 1, generally it's of wp admin -by rubix
+	$user_admin_by_rubix = 1;
 	
-	$args['user_id'] = $user_idsf;
+	//get user ids of followings of user viewing it - by rubix
+	$user_id_followings_by_rubix = bp_get_following_ids ( $user_id );
 	
-	//print_r($args);
+	//include activities from below mentioned ids collected in above statements - by rubix 
+	array_push( $user_id_by_rubix, $user_admin_by_rubix, $user_id_followings_by_rubix);
+	
+	$args['user_id_by_rubix'] = $user_id_followings_by_rubix;
+	
 	return $args;
 	
 }
-add_filter( 'bp_after_has_activities_parse_args', 'personalized_activity_for_buddypress_FwA_args' );
+add_filter( 'bp_after_has_activities_parse_args', 'personalized_activity_for_buddypress_FwA_by_rubix_args' );
